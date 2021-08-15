f3a5b28b211ad88acab7d532cd5d2179d1e99097
yuzhishuo <aoumeior@outlook.com>
2021-08-15 22:02:34 +0800

    。

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,6 +1,8 @@
 import { IDisposable } from 'yuzhi/common/lifecycle';
 import { Event, Emitter, } from 'yuzhi/common/event';
 import { IUnique } from 'yuzhi/utility/SelfDictionary';
+import { Socket } from 'net';
+import { ICycleElem } from '../utility/cycle';
 
 interface IContentHandler {
     handle(status: string): void;
@@ -15,10 +17,12 @@ interface IContentHandler {
 // 1. 通过 userId 自初始化， user 处于业务层， 它与数据库隔离。
 // 2. 通过 userId / group 找到 user / group 甚至其他的 key 能找到 类似 user group的服务
 // 3. user 不一定与 当前在线用户 一一对应 ，可以是一个离线用户，也可以是一个在线用户。 因此 它有可能具有临时状态。
-export class User<T> implements IUnique<T>, IContentHandler {
+export class User<T> implements IUnique<T>, IContentHandler, ICycleElem<T> {
 
     constructor(
-        private readonly userId: T
+        private readonly userId: T,
+        private socket: Socket,
+
     ) { }
 
 
@@ -35,12 +39,29 @@ export class User<T> implements IUnique<T>, IContentHandler {
         return this.userId;
     }
 
-    // dispose(): void {
-    //     this.keepLive !== undefined && clearTimeout(this.keepLive);
-    //     this._ononDidUserRemovedEmitter.fire(this.Unique());
-    // }
+    dispose(): void {
 
-    // private keepLive?: NodeJS.Timeout;
+    }
+}
+
+export class VirtualUser<T> implements IUnique<T>, IContentHandler, ICycleElem<T> {
+    constructor(
+        private readonly userId: T
+    ) { }
+    dispose(): void {
+        throw new Error('Method not implemented.');
+    }
+    handle(status: string): void {
+        throw new Error('Method not implemented.');
+    }
+
+    Unique(): T {
+        return this.userId;
+    }
+
+    private liveTag: NodeJS.Timeout = setTimeout(() => {
+        this.dispose();
+    }, 10000);
 }
 
 
